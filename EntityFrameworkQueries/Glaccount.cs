8cd642e887d47fcb731edab255c79ec654035e3c
Zalman Izak <thezalmanian@gmail.com>
2023-01-23 16:21:48 -0800

    Reverse engineered AP database to EF model

---
+++ EntityFrameworkQueries/Glaccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkQueries;
+
+public partial class Glaccount
+{
+    public int AccountNo { get; set; }
+
+    public string AccountDescription { get; set; } = null!;
+
+    public virtual ICollection<InvoiceLineItem> InvoiceLineItems { get; } = new List<InvoiceLineItem>();
+
+    public virtual ICollection<Vendor> Vendors { get; } = new List<Vendor>();
+}
